6f8d3bc887a57e2ea64660cbf72d7be5a8378579
Ruben Kostandyan <ruben.kostard@gmail.com>
2024-07-23 19:57:22 +0400

    Theme

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,26 +1,37 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
+    content: [
+        "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+        "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+        "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    ],
+    theme: {
+        extend: {},
+        fontFamily: {
+            body: ["Cascadia Code"],
+        },
     },
-    fontFamily: {
-      body: ["Cascadia Code"],
+    daisyui: {
+        themes: [
+            {
+                gruvbox: {
+                    primary: "#83a598",
+                    secondary: "#8ec07c",
+                    accent: "#fabd2f",
+                    neutral: "#928374",
+                    "base-100": "#282828",
+                    "base-200": "#3c3836",
+                    "base-300": "#504945",
+                    "base-content": "#fbf1c7",
+                    info: "#83a598",
+                    success: "#b8bb26",
+                    warning: "#fabd2f",
+                    error: "#fb4934",
+                },
+            },
+        ],
     },
-  },
-  daisyui: {
-    themes: ["emerald"],
-  },
-  plugins: [require("daisyui")],
+    plugins: [require("daisyui")],
 };
 export default config;
